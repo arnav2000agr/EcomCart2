5889ed324ebf02ad2cd06caf66448d9221902585
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2021-11-11 15:40:30 +0530

    updated the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
-import ecomlogo from '../ecomlogo.jpg';
+import ecomlogo2 from '../ecomlogo2.png';
 import styled from 'styled-components';
 import {ButtonContainer} from './Button';
-
 export default class Navbar extends Component {
     render() {
         return (
             <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
                <Link to='/'>
-                  <img src={ecomlogo} height={70}alt="store" className="navbar-brand" />
+                  <img src={ecomlogo2} height={70}alt="store" className="navbar-brand" />
                </Link> 
                <ul className="navbar-nav align-items-center">
                  <li className="nav-item ml-5">
                     <Link to="/" className="nav-link">
-                       <u>Our Products</u>
+                        <h4><i> Our Products </i></h4>
                     </Link>
                  </li>
                </ul>
                <Link to="/cart" className="ml-auto">
                   <ButtonContainer>
-                      <i className="fas fa-cart-plus"><b> Cart </b></i>
+                      <i className="but"><b> Cart </b></i>
                   </ButtonContainer>
                </Link>
             </NavWrapper>
