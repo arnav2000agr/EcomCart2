ea388892aa1e2a76651b35f39b41aa34e4f62fdd
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2021-11-12 00:58:00 +0530

    Updated names and prices for products

--- src/data.js
+++ src/data.js
@@ -1,30 +1,30 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "boAt Headphones",
     img: "img/product-1.png",
     price: 20000,
-    company: "GOOGLE",
+    company: "boAt",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Samsung S7",
+    title: "Apple Airpods",
     img: "img/product-2.png",
     price: 22000,
-    company: "SAMSUNG",
+    company: "Apple",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
+    title: "Samsung M12",
     img: "img/product-3.png",
     price: 23000,
-    company: "htc",
+    company: "Samsung",
   
     inCart: false,
     count: 0,
@@ -32,10 +32,10 @@ export const storeProducts = [
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "Samsung A12",
     img: "img/product-4.png",
     price: 36000,
-    company: "htc",
+    company: "Samsung",
     
     inCart: false,
     count: 0,
@@ -43,10 +43,10 @@ export const storeProducts = [
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
+    title: "Sony Bravia",
     img: "img/product-5.png",
     price: 24000,
-    company: "htc",
+    company: "Sony",
     
     inCart: false,
     count: 0,
@@ -54,10 +54,10 @@ export const storeProducts = [
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "Apple Watch",
     img: "img/product-6.png",
-    price: 17000,
-    company: "apple",
+    price: 27000,
+    company: "Apple",
     
     inCart: false,
     count: 0,
@@ -68,7 +68,7 @@ export const storeProducts = [
     title: "Iphone 11",
     img: "img/product-7.png",
     price: 56000,
-    company: "apple",
+    company: "Apple",
     
     inCart: false,
     count: 0,
@@ -76,10 +76,10 @@ export const storeProducts = [
   },
   {
     id: 8,
-    title: "Smashed Iphone",
+    title: "OnePlus TV",
     img: "img/product-8.png",
     price: 20000,
-    company: "apple",
+    company: "OnePlus",
     
     inCart: false,
     count: 0,
@@ -89,10 +89,10 @@ export const storeProducts = [
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
+  title: "boAt Headphones",
   img: "img/product-1.png",
   price: 20000,
-  company: "Google",
+  company: "boAt",
   inCart: false,
   count: 0,
   total: 0
